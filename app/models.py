b1a4f9f9dfd9e7de6dbe97dffde69412364ecd78
benzatman <benjaminzatman@gmail.com>
2025-01-14 14:26:33 +0200

    debugging

--- app/models.py
+++ app/models.py
@@ -31,12 +31,11 @@ class AbsenceLog(db.Model):
     student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     what_was_missed = db.Column(db.String(20), nullable=False)
     time_missed = db.Column(db.String(50), nullable=False)
-    reason = db.Column(db.Text, nullable=True)  # Add this line
-    details = db.Column(db.Text)
-    submission_time = db.Column(db.DateTime, default=db.func.now().op('AT TIME ZONE')('Asia/Jerusalem'))
+    reason = db.Column(db.Text, nullable=True)
+    details = db.Column(db.Text, nullable=True)
+    submission_time = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
     decision_time = db.Column(db.DateTime)
     counselor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 
 
 class ShabbatSubmission(db.Model):
